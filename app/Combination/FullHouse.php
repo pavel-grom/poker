64786a47d1c67118ee4d38a1e00e309b258edb97
Pavel Grom <dejavugromrock@gmail.com>
2018-10-19 17:06:51 +0300

    combinations determination(fix full house)

--- app/Combination/FullHouse.php
+++ app/Combination/FullHouse.php
@@ -15,4 +15,23 @@ class FullHouse extends CombinationAbstract
      * @const int WEIGHT
      * */
     public const WEIGHT = 7;
+
+    /**
+     * @return int
+     */
+    public function getTotalWeight(): int
+    {
+        $totalWeight = self::WEIGHT;
+
+        $onlyCombinationCards = $this->cards->sortByPriority();
+
+        $pairPriority = $onlyCombinationCards[0]->getWeight();
+        $setPriority = $onlyCombinationCards[$onlyCombinationCards->count() - 1]->getWeight();
+
+        $totalWeight .= $setPriority . $pairPriority;
+
+        $totalWeight .= '000000';
+
+        return (int) $totalWeight;
+    }
 }
